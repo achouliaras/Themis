cf3ff8611bf2cb74c5c517da85971e01080a38dc
achouliaras <achouliaras97@gmail.com>
2023-06-08 13:02:20 +0100

    Fixed mujoco and some hydra version issues. Omegaconf still has issues with variable interpolation

---
+++ run.sh
@@ -0,0 +1,2 @@
+seed=$RANDOM
+python train_PEBBLE.py  env=Control/Humanoid-v4 seed=$seed agent.params.actor_lr=0.0005 agent.params.critic_lr=0.0005 gradient_update=1 activation=tanh num_unsup_steps=9000 num_train_steps=500000 num_interact=20000 max_feedback=500 reward_batch=50 reward_update=50 feed_type=$1 teacher_beta=-1 teacher_gamma=1 teacher_eps_mistake=0 teacher_eps_skip=0 teacher_eps_equal=0
