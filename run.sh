25a5810f6691715738f13d64166edaf7109795cb
achouliaras <achouliaras97@gmail.com>
2023-07-06 02:07:52 +0100

    Remove video_kit folder

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 seed=$RANDOM
-#python PEBBLE_pretrain.py  domain=MiniGrid env=DistShift1-v0 render_mode=human seed=$seed num_unsup_steps=1000 num_train_steps=500000 num_interact=3000 max_feedback=1000 reward_batch=8 reward_update=100 feed_type=$1 human_teacher=False agent.batch_size=256
-python PEBBLE_train.py  domain=Control env=Humanoid-v4 render_mode=human seed=$seed num_unsup_steps=9000 num_train_steps=500000 num_interact=3000 max_feedback=1000 reward_batch=8 reward_update=100 feed_type=$1 human_teacher=True
+python PEBBLE_pretrain.py  domain=MiniGrid env=DistShift1-v0 render_mode=human seed=$seed num_unsup_steps=1000 num_train_steps=500000 num_interact=3000 max_feedback=1000 reward_batch=8 reward_update=100 feed_type=$1 human_teacher=False agent.batch_size=256
+#python PEBBLE_train.py  domain=Control env=Ant-v4 render_mode=human seed=$seed num_unsup_steps=9000 num_train_steps=500000 num_interact=3000 max_feedback=1000 reward_batch=8 reward_update=100 feed_type=$1 human_teacher=True
